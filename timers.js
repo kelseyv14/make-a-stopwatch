87b98007a42bbdb6b3d2939d94c191ba891caae7
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-27 14:47:57 -0500

    edits so far

--- timers.js
+++ timers.js
@@ -1,115 +1,77 @@
+var startButton = document.querySelector('#start'); 
+var timeDisplay = document.querySelector('#timeDisplay');
+var tenths = 0;
+var seconds = 0; 
+var  minutes = 0; 
 
-//I feel like I should have color interval and stop interval somewhere up here ?
+makeStartButtonClickable();
 
-document.querySelector('#start').addEventListener('click', function(){
-setInterval(startTimer, 10);
-});
-
-//sets the time places to 0
-
-var startClock = document.querySelector('#timer');
-
-    var tenths = 0
-    var seconds = 0 
-    var  minutes = 0 
-
- //sets the order...confused about the ++ and the math but I know it's a rule based on the tests from a few weeks ago   
-   
-function startTimer() {
-    tenths++;
-    startClock.innerHTML = `${minutes}: ${seconds}: ${tenths}`
-    if (tenths >= 99) {
-        tenths = 0;
-        seconds++;
-    }
-    else if (seconds >= 60) {
-            seconds = 0;
-            minutes++;
-    }
-    else if (minutes >= 60) {
-            //go to google
-    }
+function makeStartButtonClickable() {
+startButton.addEventListener('click', startWatch);
 }
-   
- //here is me trying to get the buttons to change  
-
- //#1
-
-// <script type="text/javascript">
-// function change()
-// {
-// document.getElementById("start").value="Pause";
-// }
-// </script>
-
-//#2
-
-// function change() 
-// {
-//     var elem = document.getElementById("start");
-//     addEventListener('click');
-//     if (elem.value=="Start") elem.value = "Pause";
-//     else elem.value = "Start";
-// }   
-
-// #3
-
-
-// function startTimer(e) {
-//     if (e.target.innerHTML===start){
-//     timer = setInterval(startTimer, 100);
-//     e.target.innerHTML = 'Pause';
-//     }
-
-
-// first attempt at changing the color changed the background on click ??
-
-
-// var seconds;
-
-// function changecolors() {
-//     seconds = 1;
-//     setInterval(change, 1000);
-// }
-
-// function change() {
-//     if (seconds === 1) {
-//         color = "red";
-//         seconds = 2;
-//     } else {
-//         color = "green";
-//         seconds = 1;
-//     }
-
-//     document.body.style.background = color;
-// }
-
-//this is what finally changed the button but I am missing two intervals
-
-document.querySelector('#start').addEventListener('click', function (e) {
-    var startButton = e.target;
-    if (startButton.innerHTML === 'Start') {
-        startButton.innerHTML = 'Pause';
-        interval = setInterval(startTimer, 10)
-        colorInterval = setInterval(colorTimer, 1000);
+function startWatch() {
+    if (startButton.innerText.trim() === "start") {
+        setInterval(updateTimer, 100);
+        changeButtonToPause();
     }
-    else if (startButton.innerHTML === 'Pause') {
-        startButton.innerHTML = 'Resume';
+     else if (startButton.innerHTML === 'pause') {
+        startButton.innerHTML = 'resume';
         clearInterval(interval);
         clearInterval(colorInterval);
         timeoutInterval = setInterval(timeout, 15000)
     }
-    else if (startButton.innerHTML === 'Resume') {
-        startButton.innerHTML = 'Pause';
+    else if (startButton.innerHTML === 'resume') {
+        startButton.innerHTML = 'pause';
         interval = setInterval(startTimer, 10)
         colorInterval = setInterval(colorTimer, 1000);
     }
+}
+
+function updateTimer() {
+    startCountingUp();
+    updateTimerDisplay();
+if (tenths === 0) {
+    changeColorOfTimer();
+}
+}
+
+function startCountingUp() {
+    tenths++;
+    if (tenths === 10) {
+        seconds++;
+        tenths = 0;
+    }
+    if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+    }
    
-});
+}
+function updateTimerDisplay() {
+timeDisplay.innerHTML = `${minutes}: ${seconds}: ${tenths}`;
+}  
+
+function changeColorOfTimer() {
+    var red = Math.round(Math.random() * 255);
+    var green = Math.round(Math.random() * 255);
+    var blue = Math.round(Math.random() * 255);
+
+    timeDisplay.style.color = `rgb(${red}, ${green}, ${blue})`;
+
+}
 
 
-//these are the items I need to make this work but I don't know what they look like or where they go
-colorTimer
-clearInterval
+function changeButtonToPause() {
+    startButton.innerHTML = 'pause';
+}
+
+
+
+ 
+   
+   
+
+
+
 
 
